767575bd23d1292abbb82f9722604cbf469dc5d2
Erik Stenvall <ess@recordedfuture.com>
2016-12-09 09:41:33 +0100

    Restructured docs

--- README.rst
+++ README.rst
@@ -33,6 +33,75 @@ Creating a client
     api = ApiClient(auth='my_token')
 
 
-.. _Examples: http:docs/examples.rst
-    
 .. _API documentation: https://github.com/recordedfuture/api/wiki/RecordedFutureAPI
+
+Entity
+^^^^^^
+
+.. code:: python
+
+    entity = api.get_entity("ME4QX") # Find the Recorded Future Entity
+    print(entity.name) # prints Recorded Future
+
+Entities
+^^^^^^^^
+
+.. code:: python
+
+    # create a generator of entities
+    entities = api.get_entities({
+        "type": "Company"
+    }, limit=20)
+    for e in entities:
+        print(e.name) # prints company names
+
+References
+^^^^^^^^^^
+
+.. code:: python
+
+    # create a generator of references
+    references = api.get_references({
+        "type": "CyberAttack"
+    }, limit=20)
+    for r in references:
+        print(r.fragment) # prints company names
+
+Raw query
+^^^^^^^^^
+
+.. code:: python
+
+    # Get QueryResponse object
+    import json
+    query_response = api.query({
+        "references": {
+            "type": "CyberAttack",
+            "limit": 20
+        }
+    })
+    # get json as dict
+    print(json.dumps(query_response.result, indent=2))
+
+Metadata
+^^^^^^^^^
+
+.. code:: python
+
+    # Get dict with metadata info
+    import json
+    metadata = api.get_metadata()
+    # get json as dict
+    print(json.dumps(metadata, indent=2))
+
+Status
+^^^^^^^^^
+
+.. code:: python
+
+    # Get API user token usage
+    import json
+    status = api.get_status()
+    # get json as dict
+    print(json.dumps(status, indent=2))
+
--- setup.py
+++ setup.py
@@ -4,19 +4,49 @@ import setuptools
 from rfapi import __version__ as rf_version
 
 URL = 'https://github.com/recordedfuture/rfapi-python'
+LONG_DESCRIPTION = """rfapi-python
+============
+
+Python library for using the Recorded Future API
+
+Recorded Future's API enables you to build analytic applications and
+perform analysis which is aware of events happening around the globe
+24x7. You can perform queries and receive results from the Recorded
+Future Temporal Analytics Engine across a vast set of events,
+entities, and time points spanning from the far past into the future.
+
+See the `API documentation <https://github.com/recordedfuture/api/wiki/RecordedFutureAPI>`__
+for further details and example usage.
+
+To install with pip run ``pip install rfapi``
+
+An API token is required to use the Recorded Future API. You can request
+a Recorded Future API token by contacting support@recordedfuture.com or
+your account representative. The easiest way to setup your program is to
+save your api token inside an enviroment variable ``RF_TOKEN``. It is
+also possible to explicitely pass a token in the constructor.
+
+Creating a client
+^^^^^^^^^^^^^^^^^
+
+.. code:: python
+
+    from rfapi import ApiClient
+    api = ApiClient()
+
+    # or explicitely
+    api = ApiClient(auth='my_token')
 
 
-def read(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as fdin:
-        result = fdin.read()
-    return result
+.. _API documentation: https://github.com/recordedfuture/api/wiki/RecordedFutureAPI
+"""
 
 setuptools.setup(
     name = 'rfapi',
     packages = ['rfapi'], # this must be the same as the name above
     version = rf_version,
     description = 'API access to the Recorded Future API.',
-    long_description = read('README.rst'),
+    long_description = LONG_DESCRIPTION,
     author = 'Recorded Future, Inc.',
     author_email = ['ess@recordedfuture.com',
                     'edkrantz@recordedfuture.com'],
