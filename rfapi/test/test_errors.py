c6def8ffa447230ac85f047db7e4c533a28b0a58
Michel Edkrantz <me@webic.se>
2017-01-24 14:23:55 +0100

    Version 1.4. Fix paging bugs. Better error handling to show custom error messages from server.

---
+++ rfapi/test/test_errors.py
@@ -0,0 +1,14 @@
+import unittest
+from ..error import JsonParseError, MissingAuthError
+
+
+class ApiClientTest(unittest.TestCase):
+    def test_json_parse_error(self):
+        resp = type('', (object,), {"content": ""})()
+        msg = "Could not parse"
+        e = JsonParseError(msg, resp)
+        self.assertEqual(str(e), msg)
+
+    def test_missing_auth_error(self):
+        e = MissingAuthError()
+        self.assertTrue("API" in str(e))
