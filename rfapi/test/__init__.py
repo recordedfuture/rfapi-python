d0863e231e082c3eca60da0b55af55da1706d0d2
Erik Ramfelt <erik.ramfelt@recordedfuture.com>
2019-08-28 12:58:09 +0200

    removed

--- rfapi/test/__init__.py
+++
@@ -1,3 +0,0 @@
-from .. import RF_API_URL, DEFAULT_API_URL
-IS_DEFAULT_API_URL = RF_API_URL == DEFAULT_API_URL
-IS_DEFAULT_API_URL_MSG = "This test requires the default api url"
--- rfapi/util.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-def snake_to_camel_case(column_name):
-    return re.sub('_([a-z])',
-                  lambda match: match.group(1).upper(),
-                  column_name)
