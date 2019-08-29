1f52291a1c884f76179398248ca76ada7fca62cb
Erik Ramfelt <erik.ramfelt@recordedfuture.com>
2019-08-29 13:55:53 +0200

    Added main py

---
+++ rfapi/main.py
@@ -0,0 +1,21 @@
+import os
+
+from rfapi import ConnectApiClient
+from argparse import ArgumentParser
+
+def iplookup(args):
+    api = ConnectApiClient(auth=args.rf_token)
+    print(api.lookup_ip(args.ipaddress))
+
+parser = ArgumentParser()
+parser.add_argument('--token', dest="rf_token", default=os.environ["RF_TOKEN"])
+subparsers = parser.add_subparsers()
+
+ipLookupParser = subparsers.add_parser('iplookup')
+ipLookupParser.add_argument('ipaddress')
+ipLookupParser.set_defaults(func=iplookup)
+
+
+if __name__ == "__main__":
+    parser_args = parser.parse_args()
+    parser_args.func(parser_args)
